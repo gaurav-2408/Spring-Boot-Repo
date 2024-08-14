e4d25d1a4b3a854b90adb9f9464c1cf5907d46ca
Gaurav Jain <gauravjain200024@gmail.com>
2024-08-14 13:44:01 +0530

    First Commit

---
+++ CRUDApplication/src/main/java/com/example/CRUDApplication/controller/BookController.java
@@ -0,0 +1,77 @@
+package com.example.CRUDApplication.controller;
+
+import com.example.CRUDApplication.model.Book;
+import com.example.CRUDApplication.repo.BookRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class BookController {
+    @Autowired
+    private BookRepo bookrepo;
+
+    @GetMapping("/getAllBooks")
+    public ResponseEntity<List<Book>> getAllBooks(){
+        try{
+            List<Book> bookList = new ArrayList<>();
+            bookrepo.findAll().forEach(bookList::add);
+
+            if(bookList.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(bookList, HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/getBookById/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id){
+        Optional<Book> bookData = bookrepo.findById(id);
+
+        if(bookData.isPresent()){
+            return new ResponseEntity<>(bookData.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/addBook")
+    public ResponseEntity<Book> addBook(@RequestBody Book book){
+        try{
+            Book bookObj = bookrepo.save(book);
+            return new ResponseEntity<>(bookObj,HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/updateBookById/{id}")
+    public ResponseEntity<Book> updateBookById(@PathVariable Long id, @RequestBody Book newBookData){
+        Optional<Book> oldBookData = bookrepo.findById(id);
+
+        if(oldBookData.isPresent()){
+            Book updatedBookData = oldBookData.get();
+            updatedBookData.setTitle(newBookData.getTitle());
+            updatedBookData.setAuthor(newBookData.getAuthor());
+
+            Book bookObj = bookrepo.save(updatedBookData);
+            return new ResponseEntity<>(bookObj,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/deleteBookById/{id}")
+    public ResponseEntity<HttpStatus> deleteBookById(@PathVariable Long id){
+        bookrepo.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
